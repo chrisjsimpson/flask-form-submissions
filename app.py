71a7b1409a83409f6766c6d6196c54d8f0ad8e73
chrisjsimpson <chris@karmacomputing.co.uk>
2021-02-22 21:53:01 +0000

    added form submission example

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,17 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup-form.html")
+
+
+@app.route("/signup", methods=["POST"])
+def save_signup():
+    data = request.form
+    return render_template("thank-you.html", data=data)
 
 
 @app.route("/about")
